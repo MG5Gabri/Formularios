45dcdea5e92af14ee6f74e129b48717170d4b77f
lglopez <lglopez@scl.edu.gt>
2024-07-24 08:12:11 -0600

    diseño principalito

---
+++ main/main.js
@@ -0,0 +1,28 @@
+let root = document.querySelector("#root");
+
+//comentario
+let div = document.createElement("div");
+div.className = "divFormularios";
+
+//lista de textos
+let formularios = [
+    {nombre:"Login", link:"https:"},
+    {nombre:"Login", link:"https:"},
+    {nombre:"Login", link:"https:"},
+    {nombre:"Login", link:"https:"},
+];
+
+//forEach
+formularios.forEach(itemLista =>{
+    //Creamos un div que se llame item
+    let divitem = document.createElement("a")
+    divitem.className = "item";
+    divitem.href = itemLista.link;
+    divitem.innerText = itemLista.nombre;
+
+    div.appendChild(divitem)
+});
+
+
+//comentario
+root.appendChild(div)
